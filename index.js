99324ba03eebfcce5014ab69f9d7d660498557e1
janChorny <janchorny@tut.by>
2023-01-18 23:55:35 +0300

    init: start task

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import { httpServer } from "./src/http_server/index.js";
 import { mouse } from "@nut-tree/nut-js";
+import WebSocket, { WebSocketServer } from "ws";
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wws = new WebSocketServer({ port: 8080 });
+
+wws.on('connection', ws => {
+  ws.on('message', data => {
+    console.log('received:', data);
+  })
+
+  ws.send('something');
+})
+
+wws.on('close', () => {
+
+})
