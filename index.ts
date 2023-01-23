a8f32a9b09396115023f5f1d96e4da87959c29d3
janChorny <janchorny@tut.by>
2023-01-23 14:52:44 +0300

    fix: change socket close

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { httpServer } from "./src/http_server/index";
-import WebSocket, { WebSocketServer } from "ws";
+import { WebSocketServer } from "ws";
 import { controller } from "./src/controller";
 
 const HTTP_PORT = 8181;
@@ -7,15 +7,25 @@ const HTTP_PORT = 8181;
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
-const wws = new WebSocketServer({ port: 8080 });
-const appAddress = 'http://localhost:8181';
+const wss = new WebSocketServer({ port: 8080 });
+const socketAddress = 'http://localhost:8181';
 
-wws.on('connection', async (ws) => {
-  console.log('Start WebSocket server on the 8080 port!');
-  console.log(`Connection ${appAddress} started!`);
-  await controller(ws);
+wss.on('listening', () => {
+  console.log('Start WebSocket server on 8080 port!');
+});
+
+wss.on('connection', async (ws) => {
+  console.log(`Connection ${socketAddress} started!`);
   ws.on('close', () => {
-    ws.close()
-    console.log(`Connection ${appAddress} ended!`);
+    console.log(`Connection ${socketAddress} stopped!`);
   });
+  await controller(ws);
 })
+
+process.on('SIGINT', () => {
+  wss.close();
+  console.log('End WebSocket server on 8080 port!');
+  httpServer.close();
+  console.log('End static http server on 8181 port!');
+  process.exit();
+});
